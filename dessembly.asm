d178916611ea49dbc76149f9a9e8d457fc9f5af3
SerjeiMikailov <felipemaders8@gmail.com>
2023-07-30 00:48:10 -0300

    first commit

---
+++ dessembly.asm
@@ -0,0 +1,156 @@
+section .data
+    s1 db 'omegalul', 0 
+
+section .text
+global _start
+
+_start:
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, s1   
+    mov rdx, 13                
+    syscall
+
+    
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, newline           
+    mov rdx, 1                 
+    syscall
+
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, s1   
+    mov rdx, 13                
+    syscall
+
+    
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, newline           
+    mov rdx, 1                 
+    syscall
+
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, s1   
+    mov rdx, 13                
+    syscall
+
+    
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, newline           
+    mov rdx, 1                 
+    syscall
+
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, s1   
+    mov rdx, 13                
+    syscall
+
+    
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, newline           
+    mov rdx, 1                 
+    syscall
+
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, s1   
+    mov rdx, 13                
+    syscall
+
+    
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, newline           
+    mov rdx, 1                 
+    syscall
+
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, s1   
+    mov rdx, 13                
+    syscall
+
+    
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, newline           
+    mov rdx, 1                 
+    syscall
+
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, s1   
+    mov rdx, 13                
+    syscall
+
+    
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, newline           
+    mov rdx, 1                 
+    syscall
+
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, s1   
+    mov rdx, 13                
+    syscall
+
+    
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, newline           
+    mov rdx, 1                 
+    syscall
+
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, s1   
+    mov rdx, 13                
+    syscall
+
+    
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, newline           
+    mov rdx, 1                 
+    syscall
+
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, s1   
+    mov rdx, 13                
+    syscall
+
+    
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, newline           
+    mov rdx, 1                 
+    syscall
+
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, s1   
+    mov rdx, 13                
+    syscall
+
+    
+    mov rax, 1                 
+    mov rdi, 1                 
+    mov rsi, newline           
+    mov rdx, 1                 
+    syscall
+
+    mov rax, 60               
+    xor rdi, rdi              
+    syscall
+
+section .data
+    newline db 10    
